4b9c1ea3e5596d389bc813f758a138e8fda4dcda
Felix <felix_2327250059@mhs.mdp.ac.id>
2024-05-17 20:32:08 +0700

    Template
    mengubah main blade dan create blade pada prodi dan fakultas

--- Felix/resources/views/fakultas/index.blade.php
+++ Felix/resources/views/fakultas/index.blade.php
@@ -4,10 +4,36 @@
      
     <h2>Daftar Fakultas</h2>
     <p>Ini halaman daftar fakultas</p>
-
-    @foreach ($fakultas as $fakulta)
-    {{ $fakulta['nama'] }} | {{ $fakulta['singkatan'] }}<br>
-        
-    @endforeach
+    <div class="row">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Daftar Fakultas</h4>
+                <p class="card-description">
+                List Data Fakultas
+                </p>
+                <a href="{{ url('fakultas/create') }}" class="btn btn-secondary btn-rounded btn-fw">Tambah</a>
+                <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                        <th>Nama Fakultas</th>
+                        <th>singkatan</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($fakultas as $fakulta)
+                        <tr>
+                        <td>{{ $fakulta['nama'] }} </td>
+                        <td>{{ $fakulta['singkatan'] }}</td>
+                        </tr>    
+                        @endforeach
+                    </tbody>
+                </table>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
--- Felix/resources/views/prodi/index.blade.php
+++ Felix/resources/views/prodi/index.blade.php
@@ -4,10 +4,36 @@
     
    <h2>Daftar Prodi</h2>
    <p>Ini halaman daftar fakultas</p>
-
-   @foreach ($prodis as $prodi)
-   {{ $prodi['nama'] }} {{ $prodi['Fakultas']['nama'] }} | {{ $prodi['Fakultas']['singkatan'] }}<br>
-       
-   @endforeach
+   <div class="row">
+      <div class="col-lg-12 grid-margin stretch-card">
+          <div class="card">
+          <div class="card-body">
+              <h4 class="card-title">Daftar Fakultas</h4>
+              <p class="card-description">
+              List Data Fakultas
+              </p>
+              <a href="{{ url('prodi/create') }}" class="btn btn-inverse-dark btn-fw">Tambah</a>
+              <div class="table-responsive">
+              <table class="table table-hover">
+                  <thead>
+                  <tr>
+                      <th>Nama Prodi</th>
+                      <th>Nama Fakultas</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                     @foreach ($prodis as $prodi)
+                     <tr> 
+                        <td>{{ $prodi['Fakultas']['singkatan'] }}</td>
+                        <td>{{ $prodi['nama'] }} {{ $prodi['Fakultas']['nama'] }} </td>
+                     </tr>      
+                     @endforeach
+                  </tbody>
+              </table>
+              </div>
+          </div>
+          </div>
+      </div>
+  </div>
 @endsection
 
