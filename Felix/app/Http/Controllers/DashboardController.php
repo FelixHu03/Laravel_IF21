b6552dfe6acb8bb6597964b8d3032e2540b70e80
Felix <felix_2327250059@mhs.mdp.ac.id>
2024-06-11 18:43:45 +0700

    menambahkan sistem login

--- Felix/app/Http/Controllers/DashboardController.php
+++ Felix/app/Http/Controllers/DashboardController.php
@@ -7,12 +7,23 @@ use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        $mahasiswaProdi = DB::select
-        ("SELECT prodis.nama, COUNT(*) as jumlah 
+    public function index()
+    {
+        $mahasiswaProdi = DB::select("SELECT prodis.nama, COUNT(*) as jumlah 
         FROM `mahasiswas` 
         JOIN prodis on mahasiswas.prodi_id = prodi_id
         GROUP by prodis.nama");
-        return view('dashboard') ->with('mahasiswaProdi', $mahasiswaProdi);
+
+        $mahasiswaJK = DB::select(
+            "SELECT prodis.nama,
+            SUM( CASE WHEN mahasiswas.jk = 'L' THEN 1 ELSE 0 END) Laki_Laki,
+            SUM(CASE WHEN mahasiswas.jk = 'P' THEN 1 ELSE 0 END) Perempuan
+            FROM mahasiswas
+            JOIN prodis on mahasiswas.prodi_id = prodi_id
+            GROUP by prodis.nama"
+        );
+        return view('dashboard')
+        ->with('mahasiswaProdi', $mahasiswaProdi)
+        ->with('mahasiswaJK', $mahasiswaJK);
     }
 }
